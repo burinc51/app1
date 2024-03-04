92a0bdede0471bbc85e2d08d55b4a6e5ebf6002c
gotzila64 <burinc51@gmail.com>
2024-03-04 10:28:50 +0700

    create form-check.js and update App.js
    create form-check.js and update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import FormText from './form-text'
+import FormCheck from './form-check'
 
 export default function App() {
-  return <FormText/>
+  return <FormCheck/>
 }
